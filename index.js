cc9f2328e64676962b8353ae8494271ac36be01f
Atharva Bhogle <atharvabhogle04@gmail.com>
2025-09-24 16:15:57 +0530

    under work

--- index.js
+++ index.js
@@ -3,19 +3,39 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "";
 
-weatherForm.addEventListener("submit", (event) => {
+weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const city = cityInput.value;
 
   if (city) {
+    try {
+      const weatherData = await getData(city);
+      display(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
   } else {
     displayError("Enter city please");
   }
 });
 
-async function getData(city) {}
+async function getData(city) {
+  const apiUrl = ``;
+  const response = await fetch(apiUrl);
+  console.log(response);
 
-function display(data) {}
+  if (!response.ok) {
+    throw new Error("Couldnt fetch weather data");
+  }
+
+  return await response.json();
+}
+
+function display(data) {
+  {
+  }
+}
 
 function emoji(weatherId) {}
 
